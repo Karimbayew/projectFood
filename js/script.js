5463c7eafc788e9481cc4e0f88e3442f702d21e6
Shahzad <karimbayewshahzad@gmail.com>
2023-06-18 16:57:58 +0500

    bindPostData

--- js/script.js
+++ js/script.js
@@ -228,52 +228,101 @@ window.addEventListener('DOMContentLoaded', () => {
     const forms = document.querySelectorAll('form');
 
     const message = {
-        loading: 'Загрузка',
-        success: 'Спасибо скоро мы с вами свяжемся',
-        failure: 'Что-то пошла не так'
-    };
+        wait: 'Подождте...',
+        success: 'Спасибо мы скоро с вами свяжемься',
+        failure: 'Что-то пошла не так пожалуйста повторите еще раз'
+    }
 
     forms.forEach(item => {
-        postData(item)
-    });
+        bindPostData(item);
+    })
+
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json()
+    };
     
-    function postData(form) {
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage); 
+            const formData = new FormData(form)
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+            // formData massivdagi massivga aylantiramiz, son obycno obyekt aylantiramiz, son JSON formata aylantyramiz
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            request.setRequestHeader('Content-type', 'application/json');
-            const formData = new FormData(form);
-
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            });
-
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if(request.status === 200) {
-                    console.log(request.response);
-                    statusMessage.textContent = message.success;
-                    form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000)
-                } else {
-                    statusMessage.textContent = message.failure;
-                }
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
+                console.log(data);
+                console.log(message.success)
+            }).catch(() => {
+                console.log(message.failure);
+            }).finally(() => {
+                form.reset();
             })
         })
     }
+
+    // fetch('http://localhost:3000/menu')
+    //     .then(data => data.json())
+    //     .then(res => console.log(res));
+
+    // const forms = document.querySelectorAll('form');
+
+    // const message = {
+    //     loading: 'Загрузка',
+    //     success: 'Спасибо скоро мы с вами свяжемся',
+    //     failure: 'Что-то пошла не так'
+    // };
+
+    // forms.forEach(item => {
+    //     postData(item)
+    // });
+    
+    // function postData(form) {
+    //     form.addEventListener('submit', (e) => {
+    //         e.preventDefault();
+
+    //         const statusMessage = document.createElement('div');
+    //         statusMessage.classList.add('status');
+    //         statusMessage.textContent = message.loading;
+    //         form.append(statusMessage); 
+
+    //         const request = new XMLHttpRequest();
+    //         request.open('POST', 'server.php');
+
+    //         request.setRequestHeader('Content-type', 'application/json');
+    //         const formData = new FormData(form);
+
+    //         const object = {};
+    //         formData.forEach(function(value, key) {
+    //             object[key] = value;
+    //         });
+
+    //         const json = JSON.stringify(object);
+
+    //         request.send(json);
+
+    //         request.addEventListener('load', () => {
+    //             if(request.status === 200) {
+    //                 console.log(request.response);
+    //                 statusMessage.textContent = message.success;
+    //                 form.reset();
+    //                 setTimeout(() => {
+    //                     statusMessage.remove();
+    //                 }, 2000)
+    //             } else {
+    //                 statusMessage.textContent = message.failure;
+    //             }
+    //         })
+    //     })
+    // }
     
 });
